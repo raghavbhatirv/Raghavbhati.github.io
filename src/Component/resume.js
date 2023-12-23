5e260d487f4a561813c84f8b0a166fb43eab3b6d
Raghavbhati <bhatiraghav4@gmail.com>
2023-12-23 19:38:44 +0530

    Portfolio code secure

--- src/Component/resume.js
+++
@@ -1,8 +0,0 @@
-import resume from "../Assets/RaghavResume.pdf"
-export const handleDwonloadResume = () => {
-    const link = document.createElement("a");
-    link.download = "RaghavBhati.pdf";
-    link.href = resume;
-    link.click();
-    window.open("https://drive.google.com/file/d/1VS0vfkt7mivJBgBKkpC9qLRbClTU6S_5/view?usp=sharing", "_blank");
-}
